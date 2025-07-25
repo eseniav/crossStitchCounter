8334593882cace34f504c95e6cea831fc1459a7e
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-07-25 14:34:50 +0300

    add code

---
+++ src/Main.kt
@@ -0,0 +1,43 @@
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+
+fun tryParseDate(str: String): LocalDate? {
+    return try {
+            LocalDate.parse(str)
+        }
+        catch(e: Exception) {
+            null
+        }
+}
+fun crossInDay(cross: Int, date: LocalDate): Int {
+    val dayDiff = ChronoUnit.DAYS.between(LocalDate.now(), date) + 1
+    return cross / dayDiff.toInt()
+}
+fun main(args: Array<String>) {
+    print("Введите общее количество крестиков: ")
+    var totalQuantity: Int? = null
+    while(totalQuantity == null) {
+        totalQuantity = readln().toIntOrNull()
+        if (totalQuantity == null || totalQuantity <= 0) {
+            totalQuantity = null
+            print("Введены неверные данные. Введите число: ")
+        }
+    }
+    print("Введите вышитое количество крестиков: ")
+    var crossQuantity = readln().toIntOrNull() ?: 0
+    crossQuantity = if (crossQuantity < 0 || crossQuantity >= totalQuantity) 0 else crossQuantity
+    var currentQuantity = totalQuantity - crossQuantity
+    print("Введите планируемую дату завершения работы (ГГГГ-ММ-ДД): ")
+    var finishDate: LocalDate? = null
+    val today = LocalDate.now()
+    while(finishDate == null) {
+        finishDate = tryParseDate(readln())
+        if (finishDate == null || finishDate.isBefore(today)) {
+            finishDate = null
+            print("Введены неверные данные. Введите дату: ")
+        }
+    }
+    println("Общее количество крестиков: $totalQuantity")
+    println("Введена дата: $finishDate")
+    println("Крестики в день: ${crossInDay(currentQuantity, finishDate)}")
+}
