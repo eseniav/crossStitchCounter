d0f4948001c05f737d4c04ddbe42c616bf758f1b
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-08-13 14:07:44 +0300

    add greeting to string overriding

--- src/App.kt
+++ src/App.kt
@@ -1,8 +1,8 @@
 abstract class App(private val greeting: Greeting) {
     fun greet() {
-        println(Console.format(decorate(greeting.message), greeting.color))
+        println(greeting)
     }
 }
-class Greeting(val message: String, val color: Console.Color) {
-
+data class Greeting(val message: String, val color: Console.Color) {
+    override fun toString() = Console.format(decorate(message), color)
 }
