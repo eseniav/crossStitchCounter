20defca729be50018617d3f39e1a63b275b93a72
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-08-20 14:02:05 +0300

    add lname, date validation

--- src/User.kt
+++ src/User.kt
@@ -10,4 +10,18 @@ class User(): Identifiable {
     val birthDate: LocalDate? = null
     val userID: String = getID()
     val regDate: LocalDate = LocalDate.now()
+    companion object {
+        val minName = 2
+        val maxName = 30
+        val minLogin = 6
+        val maxLogin = 15
+        val minAge = 10L
+        val maxAge = 100L
+        fun checkName(input: String) = input.isNotBlank() && Validation.checkLength(input.length, minName, maxName)
+        fun checkLogin(input: String) = Validation.checkWhiteSpace(input) && Validation.checkLength(input.length, minLogin, maxLogin)
+        fun checkDate(input: LocalDate): Boolean {
+            val today = LocalDate.now()
+            return Validation.checkDateRange(input, today.minusYears(maxAge), today.minusYears(minAge))
+        }
+    }
 }
