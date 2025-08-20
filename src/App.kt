71146fadc887d933fe66625aa56a17867e8a64ac
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-08-20 11:33:16 +0300

    add cancel fun

--- src/App.kt
+++ src/App.kt
@@ -1,3 +1,5 @@
+import java.time.LocalDate
+
 abstract class App(private val greeting: Greeting) {
     fun greet() {
         println(greeting)
@@ -5,8 +7,49 @@ abstract class App(private val greeting: Greeting) {
     fun login() {
         println("Логин")
     }
+    fun checkCancel(str: String) = str.lowercase() == "отмена"
+    fun handleCancel(str: String) {
+        if(checkCancel(str)) {
+            throw Exception("cancel")
+        }
+    }
     fun regester() {
-        println("Регистрация")
+        println(Console.format("Регистрация нового пользователя", Console.Color.Blue))
+        println("Для отмены регистрации и возврата в главное меню напишите слово \"отмена\"")
+        try {
+            print("Имя: ")
+            val userName = readln()
+            handleCancel(userName)
+            print("Фамилия: ")
+            val lName = readln()
+            handleCancel(lName)
+            print("Логин: ")
+            val login = readln()
+            handleCancel(login)
+            print("Пароль: ")
+            val password = readln()
+            handleCancel(password)
+            print("Повторите пароль: ")
+            val passRep = readln()
+            handleCancel(passRep)
+            print("Дата рождения(дд.мм.гггг): ")
+            val bDate = readln()
+            handleCancel(bDate)
+            print("Телефон: ")
+            val phone = readln()
+            handleCancel(phone)
+            print("Email: ")
+            val email = readln()
+            handleCancel(email)
+        }
+        catch (ex: Exception) {
+            if(ex.message == "cancel") {
+                println(Console.format("\t Отмена регистрации пользователя. Переход в главное меню", Console.Color.Grey) + "\n")
+                mainMenu()
+            } else {
+                throw ex
+            }
+        }
     }
     fun exit() {
         println("Выход")
